295feebab921000da19283cb5403c1552c0eba8e
Yarden <yardenron4@gmail.com>
2017-07-15 14:30:58 +0300

    Adding winston logger to the app

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = require('./appRoutes');
 const config = require('./config');
+const logger = require('./logger');
 
 const app = express();
 app.use('/hello', router);
 
 const port = process.env.port || config.port;
 app.listen(port);
-console.log("Server is up listenning on port 8080");
+logger.log('info',"Server is up listenning on port 8080");
