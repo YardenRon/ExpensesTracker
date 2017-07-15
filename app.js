e646cfb4fbbd60d346ca3012ad35ea4306b21346
Yarden <yardenron4@gmail.com>
2017-07-15 14:22:17 +0300

    Adding basic router and config to the app

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const router = require('./appRoutes');
+const config = require('./config');
+
+const app = express();
+app.use('/hello', router);
+
+const port = process.env.port || config.port;
+app.listen(port);
+console.log("Server is up listenning on port 8080");
